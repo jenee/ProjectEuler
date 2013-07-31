bd8f8590225119a872c8d6a088d65ed84e492343
Jenee <jenee.hughes@gmail.com>
2013-07-30 17:10:32 -0700

    Initial commit for learning Python with Project Euler

---
+++ 001/multiplesOf3and5.py
@@ -0,0 +1,13 @@
+# http://projecteuler.net/problem=1
+# If we list all the natural numbers below 10 that are multiples 
+#   of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+# sum variable is 0
+sum = 0
+
+# loop i through 1 to < 1000
+# # if  i % 3 is 0 or i % 5 is zero
+# # # add i
+
+# print sum
