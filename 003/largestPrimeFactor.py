50dede71ba245723a8468b3dc04541569d45beca
Jenee <jenee.hughes@gmail.com>
2013-07-31 17:46:52 -0700

    Project euler problem 3
    it's the wrong solution.

---
+++ 003/largestPrimeFactor.py
@@ -0,0 +1,37 @@
+'''
+http://projecteuler.net/problem=3
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+'''
+
+
+# curDivisor = 2
+curDivisor = 2
+
+# baseNum = 600851475143
+baseNum = 13195
+
+# maxPrimeFactor = 2
+maxPrimeFactor = 2
+
+# minMaxPrimeFactor = 600851475143 / 2
+minMaxPrimeFactor = baseNum / 2
+
+while curDivisor < minMaxPrimeFactor and baseNum > minMaxPrimeFactor:
+   if baseNum % curDivisor == 0:
+      tempBase = baseNum / curDivisor
+      if curDivisor > maxPrimeFactor:
+         maxPrimeFactor = curDivisor
+      baseNum = tempBase
+      curDivisor = 2
+   else:
+      curDivisor += 1
+   
+print maxPrimeFactor
+
+
+
+
+
