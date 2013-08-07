c02fd5da31740abb59a58d251f296907bdc41e56
Jenee <jenee.hughes@gmail.com>
2013-08-06 21:12:44 -0700

    So...too slow :\
    Gotta figure out a faster way to solve this!

---
+++ 010/summationOfPrimes.py
@@ -0,0 +1,28 @@
+'''
+http://projecteuler.net/problem=10
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+'''
+def isPrime(num):
+   numIsPrime = True
+   for i in range(2,num):
+      if num % i == 0:
+         return False
+   return numIsPrime
+
+
+maxNum = 2000000
+allPossiblePrimes = range(2,maxNum)
+
+sumOfPrimes = 0
+
+for possiblePrime in allPossiblePrimes:
+   if isPrime(possiblePrime):
+      sumOfPrimes += possiblePrime
+      print possiblePrime
+      for multiple in allPossiblePrimes[possiblePrime::possiblePrime]:
+         allPossiblePrimes.remove(multiple)
+      print len(allPossiblePrimes)
+print sumOfPrimes
